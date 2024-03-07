c9857978ed0ffc8d6c0ddd98c4bfe41c77b2dc93
olya <fgfg>
2024-03-08 02:37:57 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, 
+// сумма цифр которого чётная.
+
+void Main()
+{
+    InputNumbers();
+}
+
+Main();
+
+void InputNumbers()
+{
+    for(;;)
+    {
+        Console.Write("Введите целое число: ");
+        string str = Console.ReadLine();
+        if(str != "q")
+        {
+            int num = Convert.ToInt32(str);
+            int sum = num;
+            while(num >= 10)
+            {
+                num /= 10;
+                sum += num;
+            }
+            sum += num;
+            if(sum % 2 == 0) break;
+        }
+        else break;
+    }
+    
+}
